579c89fb38e57446bfc20291de70db82b6239a10
Nnobius <jackoskelle@gmail.com>
2018-02-27 20:32:20 +0700

    chess

---
+++ chess.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,j;
+	
+	char chess[8][8] = {
+		"RNBQKBNR",
+		"PPPPPPPP",
+		"________",
+		"________",
+		"________",
+		"________",
+		"pppppppp",
+		"rnbqkbnr",
+	 };
+	 
+	printf("\n");
+	
+	for (i = 0; i < 8; i++){
+		for (j = 0; j < 8; j++){
+			printf("%c", chess[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
+
