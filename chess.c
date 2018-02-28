3754dbb55094b825bef4c971a008d1bdda6eede0
Бедин Михаил Николаевич <iv721s02@mwp09.csc.local>
2018-02-28 09:32:39 +0700

    update №3

--- chess.c
+++ chess.c
@@ -5,23 +5,23 @@ int main()
     int i,j;
 
     char chess[8][8] = {
-	"RNBQKBNR",
-	"PPPPPPPP",
-	"________",
-	"________",
-	"________",
-	"________",
-	"pppppppp",
-	"rnbqkbnr",
+        "RNBQKBNR",
+        "PPPPPPPP",
+        "________",
+        "________",
+        "________",
+        "________",
+        "pppppppp",
+        "rnbqkbnr",
     };
 
     printf("\n");
 
     for (i = 0; i < 8; i++){
-	for (j = 0; j < 8; j++){
-	    printf("%c", chess[i][j]);
-	}
-	printf("\n");
+        for (j = 0; j < 8; j++){
+            printf("%c", chess[i][j]);
+            }
+        printf("\n");
     }
 
     printf("\n");
